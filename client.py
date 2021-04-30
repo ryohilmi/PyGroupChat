eb7ea14bb56a8553c82caf1fe85096e15fd4de8d
Ryo Hilmi <ryodm.74@gmail.com>
2021-04-30 19:27:18 +0700

    Add receieve and sen chat for client

--- client.py
+++ client.py
@@ -1,9 +1,29 @@
-import socket
+import socket, threading
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+def receieve_data(sock):
+  while True:
+    try:
+      data, address = sock.recvfrom(1024)
+      print(data.decode("utf-8"))
+    except:
+      pass
 
-s.sendto(bytes("Hi server", "utf-8"), ("127.0.0.1", 2209))
+def run_client():
+  host = "127.0.0.1"
+  port = 2209
 
-message, address = s.recvfrom(1024)
+  s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-print(message.decode("utf-8"))
+  name = input("Insert your name here : ")
+
+  recv_thread = threading.Thread(target=receieve_data, args=(s, ))
+  recv_thread.start()
+
+  while True:
+    message = input()
+
+    data = f"{name} : {message}"
+
+    s.sendto(data.encode("utf-8"), (host, port))
+
+run_client()
